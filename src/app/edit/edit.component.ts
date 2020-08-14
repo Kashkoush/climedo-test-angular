b8b07f3644d31247eee63a48998c6f95cfe4ba48
Ahmed Kashkoush <kashkoush@e-phunk.com>
2020-08-14 15:45:23 +0200

    edit departments added

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ContentService } from '../content.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -10,19 +11,31 @@ import { ContentService } from '../content.service';
 export class EditComponent implements OnInit {
   EditDepartment: FormGroup
 
-  constructor(private contentService: ContentService) { }
+  constructor(private contentService: ContentService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
 
-    const editedDepartment =
+    const departmentName = this.route.snapshot.params['name'];
+    const editedDepartment = this.contentService.showDepartment(departmentName);
 
     this.EditDepartment = new FormGroup({
-      'name': new FormControl(null, Validators.required),
-      'APIKey': new FormControl(null, Validators.required),
-      'contactName': new FormControl(null, Validators.required),
-      'contactEmail': new FormControl(null, [Validators.required, Validators.email]),
-      'contactPhone': new FormControl(null, Validators.required),
+      'name': new FormControl(editedDepartment.name, Validators.required),
+      'APIKey': new FormControl(editedDepartment.APIKey, Validators.required),
+      'contactName': new FormControl(editedDepartment.contactName, Validators.required),
+      'contactEmail': new FormControl(editedDepartment.contactEmail, [Validators.required, Validators.email]),
+      'contactPhone': new FormControl(editedDepartment.contactPhone, Validators.required),
     })
+  }
+
+  sendForm() {
+    this.EditDepartment.markAllAsTouched();
+    if (this.EditDepartment.status === "VALID") {
+      // console.log(this.addDepartment.value);
+      this.contentService.deleteDepartment(this.route.snapshot.params['index']);
+      this.contentService.addDepartment(this.EditDepartment.value);
+      this.EditDepartment.reset();
+      this.router.navigate(['']);
+    }
   }
 
 }
